06cf14ace4964b77deeea57815fe27e4162a1205
Ken Youens-Clark <kyclark@Shus-MacBook-Pro.local>
2015-02-22 10:07:50 -0700

    almost working

--- Kmerizer.hs
+++ Kmerizer.hs
@@ -4,16 +4,19 @@ module Main where
 
 import Bio.Core.Sequence 
 import Bio.Sequence.Fasta 
-import Data.List
-import System.Console.CmdArgs
-import Text.Printf (printf)
 import Data.Foldable (forM_)
+import Data.List (tails)
+import Data.Stringable (toString)
+import System.Console.CmdArgs
+import System.IO 
+import System.FilePath.Posix (joinPath, takeBaseName)
+import Text.Printf (printf)
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as BC
 
 -- # --------------------------------------------------
 data Options = Options { 
-    inputFile :: String, 
+    inputFile :: Maybe String, 
     outDir    :: String,
     kmerSize  :: Int 
 } deriving (Data, Typeable, Show)
@@ -21,7 +24,7 @@ data Options = Options {
 options = Options { 
       inputFile = ""  &= typ "FILE" &= help "Input file"
     , outDir    = "." &= typ "DIR"  &= help "Output directory"
-    , kmerSize  = 20               &= help "k-mer size (20)"
+    , kmerSize  = 20                &= help "k-mer size (20)"
  } 
  &= summary "create k-mer/location files for a given FASTA file"
  &= program "kmerizer"
@@ -29,37 +32,35 @@ options = Options {
 -- # --------------------------------------------------
 kmerize :: BioSeq a => Int -> a -> ([Char],[[Char]])
 kmerize n seq =
-    ( BC.unpack $ unSL $ seqid seq,
-      findKmers n $ BC.unpack $ unSD $ seqdata seq
+    ( toString $ seqid seq,
+      findKmers n $ toString $ seqdata seq
     )
 
 -- # --------------------------------------------------
 findKmers :: Int -> [Char] -> [[Char]]
-findKmers n = filter (\s -> length s == n) . map (take n) . tails
+findKmers n = takeWhile (\s -> length s == n) . map (take n) . tails
 
 -- # --------------------------------------------------
 main = do
-    opts  <- cmdArgs $ options
-    inFile <- inputFile opts
-    print inFile 
+    opts  <- cmdArgs options
+
+    let inFile       = inputFile opts
+    let outputDir    = outDir opts
+    let baseName     = takeBaseName inFile
+    let outFileKmers = joinPath [outputDir, baseName ++ ".kmers"]
+    let outFileLoc   = joinPath [outputDir, baseName ++ ".locs"]
+
     input <- readFasta inFile
-    -- let outdir = outDir opts
-    -- print outdir ++ inFile
+    let kmers = map (kmerize (kmerSize opts)) input
 
-    -- let kmers = map (kmerize (kmerSize opts)) input
+    withFile outFileLoc WriteMode (\h -> do
+        forM_ kmers $ \(i, k) ->
+           hPutStrLn h $ printf "%s\t%d" i (length k)
+        )
 
-    {-
-    let counts = map (\(id, k) -> id ++ "\t" ++ show (length k)) kmers
-    mapM_ putStrLn counts
-
-    let mers = map (\(i,k) -> show i ++ "\t" ++ k) $ zip [1..] (concat $ map (\(id, k) -> k) kmers) 
-    mapM_ putStrLn mers
-
-    forM_ kmers $ \(i, k) ->
-       putStrLn $ printf "%s\t%d" i (length k)
-    -}
-
-    -- forM_ (zip [1..] (concatMap snd kmers)) $
-    --     putStrLn . uncurry (printf "%d\t%s")
+    withFile outFileKmers WriteMode (\h -> do
+        forM_ (zip [1..] (concatMap snd kmers)) $ \(n, kmer) ->
+           hPutStrLn h $ printf "%s\t%s" (show n) kmer
+        )
 
     putStrLn "done"
