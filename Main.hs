270c317d0ed21e0813b4f018c409c98dbe7cdacf
Ken Youens-Clark <kyclark@gmail.com>
2015-03-10 08:46:35 -0700

    cleaning up, used hlint, no longer need fasta format on kmers file

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Bio.Core.Sequence
 import Bio.Sequence.Fasta
-import Control.Monad (when)
+import Control.Monad (unless)
 import Data.Foldable (forM_)
 import Data.List (tails)
 import Data.Monoid
@@ -15,52 +15,51 @@ import Text.Printf (printf)
 
 -- # --------------------------------------------------
 data Options = Options {
-    inputFile :: String,
-    outDir    :: String,
-    kmerSize  :: Int
+  inputFile :: String,
+  outDir    :: String,
+  kmerSize  :: Int
 } deriving (Show)
 
 -- # --------------------------------------------------
-kmerize :: BioSeq a => Int -> a -> ([Char], Int, [[Char]])
+kmerize :: BioSeq a => Int -> a -> (String, Int, [String])
 kmerize n seq = (seqID, numKmers, kmers)
  where seqID = toString . seqid $ seq
        (numKmers, kmers) = findKmers n . toString . seqdata $ seq
 
 -- # --------------------------------------------------
-findKmers :: Int -> [Char] -> (Int, [[Char]])
-findKmers k cs = (n - k + 1, findKmers' n k cs)
- where n = length cs
+findKmers :: Int -> String -> (Int, [String])
+findKmers k xs = (n, findKmers' n k xs)
+ where n = length xs - k + 1
        findKmers' l k xs
-         | l >= k = take k xs : findKmers' (l - 1) k (tail xs)
+         | l > 0 = take k xs : findKmers' (l - 1) k (tail xs)
          | otherwise = []
 
 -- # --------------------------------------------------
 runWithOptions :: Options -> IO ()
 runWithOptions opts = do
-    let inFile       = inputFile opts
-    let outputDir    = outDir opts
-    let baseName     = takeBaseName inFile
-    let outFileKmers = joinPath [outputDir, baseName ++ ".kmers"]
-    let outFileLoc   = joinPath [outputDir, baseName ++ ".locs"]
+  let inFile       = inputFile opts
+  let outputDir    = outDir opts
+  let baseName     = takeBaseName inFile
+  let outFileKmers = joinPath [outputDir, baseName ++ ".kmers"]
+  let outFileLoc   = joinPath [outputDir, baseName ++ ".loc"]
 
-    outdirExists <- doesDirectoryExist outputDir
-    when (not outdirExists) (createDirectory outputDir)
+  outdirExists <- doesDirectoryExist outputDir
+  unless outdirExists (createDirectory outputDir)
 
-    input <- readFasta inFile
-    let kmers = map (kmerize (kmerSize opts)) input
+  input <- readFasta inFile
+  let kmers = map (kmerize (kmerSize opts)) input
 
-    withFile outFileLoc WriteMode (\h -> do
-        forM_ kmers $ \(kmerID, numKmers, _) ->
-           hPutStrLn h $ printf "%s\t%d" kmerID numKmers
-        )
+  locFh  <- openFile outFileLoc   WriteMode
+  kmerFh <- openFile outFileKmers WriteMode
 
-    withFile outFileKmers WriteMode (\h -> do
-        forM_ (zip [1..] (concatMap thrd kmers)) $ \(n, kmer) ->
-           hPutStrLn h $ printf "%s\t%s" (show n) kmer
-        )
+  forM_ kmers $ \(readId, numKmers, readKmers) -> do
+    hPutStrLn locFh $ printf "%s\t%d" readId numKmers
+    mapM_ (hPutStrLn kmerFh) readKmers
 
-    putStrLn $ printf "Done, wrote files to '%s'" outputDir
- where thrd (a,b,c) = c
+  hClose locFh
+  hClose kmerFh
+
+  putStrLn $ printf "Done, wrote files to '%s'" outputDir
 
 -- # --------------------------------------------------
 main :: IO ()
